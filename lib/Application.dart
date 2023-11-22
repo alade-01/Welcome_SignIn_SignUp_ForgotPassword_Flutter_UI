5810351791ec965504ccba5c7f371a83b5efa1f6
alade-01 <aladewilliame1998@gmail.com>
2023-11-22 01:01:00 -0500

    clear application file

--- lib/Application.dart
+++ lib/Application.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:onboarding_login_register_app_ui/core/RouterGenerator.dart';
-import 'package:onboarding_login_register_app_ui/core/constants.dart';
 import 'package:onboarding_login_register_app_ui/core/state_manager/ThemeBloc.dart';
 
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
@@ -36,32 +33,15 @@ class ApplicationWithTheme extends StatelessWidget {
       ],
       child: BlocBuilder<ThemeCubit, ThemeData>(
         builder: (context, theme) {
-          return AnnotatedRegion<SystemUiOverlayStyle>(
-            value: SystemUiOverlayStyle(
-              statusBarColor: primaryColor,
-              statusBarBrightness: Brightness.dark,
-              systemNavigationBarIconBrightness: Brightness.dark,
-              statusBarIconBrightness: Brightness.dark,
-              systemNavigationBarColor: Colors.white,
-            ),
-            child: MaterialApp(
-              localizationsDelegates: const [
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-              ],
-              //navigatorKey: locator<NavigationService>().navigatorKey,
-              //supportedLocales: translate.supportedLocales(),
-              //title: translate.text("app_name"),
-              title: "GoodJob",
-              theme: theme,
-              debugShowCheckedModeBanner: false,
-              initialRoute: RouterGenerator.splashRoute,
-              navigatorObservers: [routeObserver],
-              onGenerateRoute: (RouteSettings setting) {
-                return RouterGenerator.navigate(setting);
-              },
-            ),
+          return MaterialApp(
+            title: "GoodJob",
+            theme: theme,
+            debugShowCheckedModeBanner: false,
+            initialRoute: RouterGenerator.splashRoute,
+            navigatorObservers: [routeObserver],
+            onGenerateRoute: (RouteSettings setting) {
+              return RouterGenerator.navigate(setting);
+            },
           );
         },
       ),
